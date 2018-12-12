03882b118643abaacfa92ac26b58155a734727a2
JiahangOK <2973493373@qq.com>
2018-12-12 09:28:43 +0800

    update project.

--- app/src/main/java/edu/bjtu/gymclub/wezone/LoginActivity.java
+++ app/src/main/java/edu/bjtu/gymclub/wezone/LoginActivity.java
@@ -3,11 +3,11 @@ package edu.bjtu.gymclub.wezone;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
     }
 }
